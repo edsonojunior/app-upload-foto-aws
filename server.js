d304b46e8e34b8e69513baed75add71861d1eaa0
Edson <edson.oliveira@outlook.com>
2020-03-03 16:19:26 -0300

    commit

--- server.js
+++ server.js
@@ -3,19 +3,11 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
-//const http = express.createServer();
 
-// http.get('*', function (req, res) {
-//     res.redirect('https://' + req.headers.host + req.url);
-// })
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/upload-photo-aws'));
-//app.use(express.static(path.join(__dirname, 'dist', 'guardpv-web')))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/upload-photo-aws/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(port)
